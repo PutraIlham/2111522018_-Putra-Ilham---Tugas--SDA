ccca0d9b9402e69935512af15dd8b7a7c0eb807e
Putra Ilham <100454812+PutraIlham@users.noreply.github.com>
2022-02-26 20:46:20 +0700

    Add files via upload

---
+++ Tugas1SDA/Soalno4.java
@@ -0,0 +1,18 @@
+package Tugas1SDA;
+
+import java.util.ArrayList;
+
+public class Soalno4 {
+
+public static void main(String[] args) {
+ ArrayList<String> nama = new ArrayList<String>();  
+    nama.add("l");
+    nama.add("h");
+    nama.add("a");
+    nama.add("m");
+
+    System.out.println(nama.indexOf("a"));
+    System.out.println(nama.indexOf("c"));
+    System.out.println(nama.indexOf("q")); 
+    }   
+}
